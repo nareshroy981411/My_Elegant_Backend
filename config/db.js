d52e4401283ca67288a94db927eb972e9a3f7ae1
nareshroy981411 <bodasunaresh226@gmail.com>
2024-12-24 19:29:20 +0530

    clusterDB

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/");
+    await mongoose.connect("mongodb+srv://bodhasnaresh98:dYyPT9OKKnWMk3zG@cluster1.ayv3h.mongodb.net/");
     console.log('MongoDB connected');
   } catch (err) {
     console.error('MongoDB connection error:', err);
