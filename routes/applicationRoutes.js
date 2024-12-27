0afd39aec7109c98df3264ba9b132ecc2710d282
manikantha608 <manikantha608@gmail.com>
2024-12-28 04:42:51 +0530

    add addtional routes getJobsByCategory,getJobsByCompany and getApplicationByCompany

--- routes/applicationRoutes.js
+++ routes/applicationRoutes.js
@@ -2,11 +2,12 @@
 const express = require('express');
 const router = express.Router();
 const { authenticateToken, isCompany, isUser } = require('../middlewares/authMiddleware');
-const { applyJob, getApplicants, getAppliedJobs } = require( "../controllers/applicationController");
+const { applyJob, getApplicants, getAppliedJobs, getApplicationByCompany } = require( "../controllers/applicationController");
 
 router.get("/user-apply/:id",authenticateToken,isUser,applyJob);
 router.get("/user-appliedJobs",authenticateToken,getAppliedJobs);
-router.get("/company-getApplicants",authenticateToken,isCompany,getApplicants);
+router.get("/getApplicants",authenticateToken,getApplicants);
+router.get("/getApplicationByCompany",authenticateToken,isCompany,getApplicationByCompany)
 
 module.exports = router;
 
--- routes/jobRoutes.js
+++ routes/jobRoutes.js
@@ -1,8 +1,8 @@
 // jobRoutes.js
 
 const express = require('express');
-const { authenticateToken, isCompany } = require('../middlewares/authMiddleware');
-const { createJob, getJobs, getJobById, deleteJob } = require('../controllers/jobController');
+const { authenticateToken, isCompany, authorizeRole, isUser } = require('../middlewares/authMiddleware');
+const { createJob, getJobs, getJobById, deleteJob, getJobsByCategory, getJobsByCompany } = require('../controllers/jobController');
 
 const router = express.Router();
 
@@ -10,6 +10,9 @@ router.post("/createJob",authenticateToken,isCompany,createJob)
 router.get("/allJobs",authenticateToken,getJobs)
 router.get("/getJob/:id",authenticateToken,getJobById)
 router.delete('/job/:id',authenticateToken,isCompany,deleteJob)
+
+router.get("/getJobsByCategory",authenticateToken,isUser,getJobsByCategory)
+router.get("/getJobsByCompany",authenticateToken,isCompany,getJobsByCompany)
 
 
 module.exports = router;
